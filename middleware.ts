0ce8b06d2b915a37c686f78c02d849b2265ab252
Atikur Satter Mondal <atikursattermondal@gmail.com>
2024-06-09 15:39:36 +0530

    add api/webhoom to public routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,20 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/api/webhook",
+  "/question/:id",
+  "/tags",
+  "/tags/:id",
+  "/question/:id ",
+  "/community",
+  "/jobs",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isPublicRoute(req)) return; // if it's a public route, do nothing
+  auth().protect(); // for any other route, require auth
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
